477d695ad70fc0fc0e354264153a8cff748d3a33
Sandrudev <89619654+Sandrudev@users.noreply.github.com>
2024-10-05 00:59:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import asyncio
 from telethon import TelegramClient
 
-# Введите ваши api_id и api_hash
-api_id = '22328650'
+# Ваши api_id и api_hash
+api_id = 22328650
 api_hash = '20b45c386598fab8028b1d99b63aeeeb'
 
 client = TelegramClient('session_name', api_id, api_hash)
@@ -29,16 +29,12 @@ async def main(phone, code, password):
             st.error("Пожалуйста, введите имя чата.")
 
 if __name__ == "__main__":
-    if "loop" not in st.session_state:
-        st.session_state.loop = asyncio.new_event_loop()
-    
     phone = st.text_input("Введите номер телефона:")
     code = st.text_input("Введите код подтверждения:")
     password = st.text_input("Введите пароль (если есть):", type="password")
 
     if st.button("Авторизоваться"):
-        asyncio.set_event_loop(st.session_state.loop)
         try:
-            st.session_state.loop.run_until_complete(main(phone, code, password))
+            asyncio.run(main(phone, code, password))
         except Exception as e:
             st.error(f"Произошла ошибка: {e}")
