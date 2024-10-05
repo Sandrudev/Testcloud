6e35e041fb5aca767b2bdffb1c9c9e5f51392f59
Sandrudev <89619654+Sandrudev@users.noreply.github.com>
2024-10-05 11:02:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -9,28 +9,29 @@ api_id = st.text_input("Введите API ID", value='22328650')
 api_hash = st.text_input("Введите API Hash", value='20b45c386598fab8028b1d99b63aeeeb')
 group_username = st.text_input("Введите имя группы", value='maskmedic')
 
-client = TelegramClient('session_name', api_id, api_hash)
+# Убедитесь, что файл сессии сохраняется в безопасном месте
+session_name = 'session_name'  # Имя файла сессии
 
 async def fetch_participants():
-    await client.start()
-    participants = []
-    offset = 0
-    limit = 100
+    async with TelegramClient(session_name, api_id, api_hash) as client:
+        participants = []
+        offset = 0
+        limit = 100
 
-    while True:
-        chunk = await client(GetParticipantsRequest(
-            group_username,
-            ChannelParticipantsSearch(''),
-            offset,
-            limit,
-            hash=0
-        ))
-        if not chunk.users:
-            break
-        participants.extend(chunk.users)
-        offset += len(chunk.users)
+        while True:
+            chunk = await client(GetParticipantsRequest(
+                group_username,
+                ChannelParticipantsSearch(''),
+                offset,
+                limit,
+                hash=0
+            ))
+            if not chunk.users:
+                break
+            participants.extend(chunk.users)
+            offset += len(chunk.users)
 
-    return participants
+        return participants
 
 def main():
     if st.button("Получить участников"):
