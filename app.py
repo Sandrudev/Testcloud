ddbd487568107a75af3d2e2b56cc00cd93fac9a8
Sandrudev <89619654+Sandrudev@users.noreply.github.com>
2024-10-06 19:52:47 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ api_id = '22328650'
 api_hash = '20b45c386598fab8028b1d99b63aeeeb'
 session_name = 'session_name'
 
-# Создаем клиент вне основного потока
-client = TelegramClient(session_name, api_id, api_hash)
-
 async def fetch_participants(group_username):
+    client = TelegramClient(session_name, api_id, api_hash)
     await client.start()
+
     participants = []
     offset = 0
     limit = 100
@@ -29,7 +28,8 @@ async def fetch_participants(group_username):
             break
         participants.extend(chunk.users)
         offset += len(chunk.users)
-    
+
+    await client.disconnect()
     return [user.username for user in participants]
 
 def main():
@@ -38,11 +38,9 @@ def main():
 
     if st.button("Получить участников"):
         if group_username:
-            # Создаем новый цикл событий и запускаем корутину
-            loop = asyncio.new_event_loop()  # Создаем новый event loop
-            asyncio.set_event_loop(loop)     # Устанавливаем его
             try:
-                usernames = loop.run_until_complete(fetch_participants(group_username))
+                # Используем asyncio.run для корректного запуска корутин
+                usernames = asyncio.run(fetch_participants(group_username))
                 st.write("Никнеймы участников:")
                 for username in usernames:
                     st.write(username)
