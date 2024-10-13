52404de26ab695d8fa6c20a7debebd816a1a6593
Sandrudev <89619654+Sandrudev@users.noreply.github.com>
2024-10-13 20:04:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -41,14 +41,18 @@ async def process_users(source_group, target_channel):
         except Exception as e:
             print(f"Пропущен {user.username}: {e}")
 
-st.title("Пригласитель пользователей Telegram")
+def main():
+    st.title("Пригласитель пользователей Telegram")
 
-source_group_username = st.text_input("Введите юзернейм группы для парсинга:")
-target_channel_username = st.text_input("Введите юзернейм канала для добавления:")
+    source_group_username = st.text_input("Введите юзернейм группы для парсинга:")
+    target_channel_username = st.text_input("Введите юзернейм канала для добавления:")
 
-if st.button("Начать приглашение"):
-    if source_group_username and target_channel_username:
-        asyncio.run(process_users(source_group_username, target_channel_username))
-        st.success("Процесс приглашения начат!")
-    else:
-        st.error("Пожалуйста, заполните оба поля.")
+    if st.button("Начать приглашение"):
+        if source_group_username and target_channel_username:
+            asyncio.run(process_users(source_group_username, target_channel_username))
+            st.success("Процесс приглашения начат!")
+        else:
+            st.error("Пожалуйста, заполните оба поля.")
+
+if __name__ == "__main__":
+    main()
